45a8e5d49ee7a4cb7463e0cf1c1de2badad5fffd
Kairat Ubukulov <ubukulov@gmail.com>
2024-01-09 17:54:37 +0600

    fx

--- app/Models/User.php
+++ app/Models/User.php
@@ -116,9 +116,4 @@ class User extends Authenticatable
 
         return $cards;
     }
-
-    public function getBalance()
-    {
-        return Payment::where(['partner_id' => Auth::id(), 'pg_status' => 'ok'])->sum('amount');
-    }
 }
