012f3e0eab1a47c600c7b8363024f770ea5cfabb
Kairat Ubukulov <ubukulov@gmail.com>
2024-03-05 17:32:45 +0500

    fx

--- resources/views/partner/qr.blade.php
+++ resources/views/partner/qr.blade.php
@@ -15,24 +15,22 @@
                 @php
                     $imgUrl = "/qrcodes/" . $user_profile->company . "_qr_code.svg";
                 @endphp
-                {{--<img src="/images/cabinet/Bitmap.svg" alt="qrcode">--}}
-{{--                {!! QrCode::size(201)->generate(route('paymentPage', ['slug' => $user_profile->category->slug, 'id' => $user_profile->user_id])); !!}--}}
-{{--                <img src="{!!QrCode::format('png')->generate(route('paymentPage', ['slug' => $user_profile->category->slug, 'id' => $user_profile->user_id]), 'QrCode.png', 'image/png')!!}">--}}
+                {!! QrCode::size(201)->generate(route('paymentPage', ['slug' => $user_profile->category->slug, 'id' => $user_profile->user_id])); !!}
                 {!! QrCode::size(201)->generate(route('paymentPage', ['slug' => $user_profile->category->slug, 'id' => $user_profile->user_id]), public_path() . $imgUrl); !!}
             </div>
             <div class="myqr__block">
                 <div class="myqr__right">
-                    <button class="myqr__download-qrcode">
+                    <a href="{{ asset($imgUrl) }}" target="_blank" class="myqr__download-qrcode">
                         <img src="/images/cabinet/download.svg" alt="icon">
                         скачать QR код
-                    </button>
+                    </a>
                     <div class="myqr__right-info">Используйте QR для создания своего маркетинг контента</div>
                 </div>
                 <div class="myqr__left">
-                    <a href="{{ asset($imgUrl) }}" target="_blank" class="myqr__download-booklet">
+                    <button class="myqr__download-booklet">
                         <img src="/images/cabinet/booklet-icon.svg" alt="icon">
                         скачать буклет
-                    </a>
+                    </button>
                     <div class="myqr__left-info">Используйте наш готовый буклет для запуска своей акции</div>
                     <div class="myqr__left-links">
                         <a href="#" class="myqr__left-link-stol">на стол</a>
