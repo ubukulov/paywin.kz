482afe188f27812053da744083606baca9fab689
Kairat Ubukulov <ubukulov@gmail.com>
2024-03-31 04:47:10 +0500

    fx

--- app/Models/User.php
+++ app/Models/User.php
@@ -55,8 +55,7 @@ class User extends Authenticatable
     public function shares()
     {
         return $this->hasMany(Share::class)
-            ->whereDate('to_date', '>=', Carbon::now())
-            ->where('cnt', '>', 0);
+            ->whereDate('to_date', '>=', Carbon::now());
     }
 
     public function create_profile()
