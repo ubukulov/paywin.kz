81e60872da14e657a0b101a90a9dd48f35518661
Kairat Ubukulov <ubukulov@gmail.com>
2024-04-02 21:50:41 +0500

    fx

--- public/b5/js/script.js
+++ public/b5/js/script.js
@@ -2,7 +2,7 @@ var prize_give = document.getElementById('prize_give');
 var dis_b = document.getElementById('dis_b');
 var suc = document.getElementById('suc');
 
-prize_give.addEventListener('click', (e) => {
+prize_give.addEventListener('click', function() {
     dis_b.style.display = 'none';
     suc.style.display = 'block';
 });
