06e02d24e70ac33b28be2f9f1e91ddabe1b59c45
Kairat Ubukulov <ubukulov@gmail.com>
2025-07-07 07:47:44 +0500

    fx

--- resources/views/_partials/info.blade.php
+++ resources/views/_partials/info.blade.php
@@ -4,8 +4,11 @@
     <div>
         <p>&copy; Paywin.kz 2024</p>
         <p><a style="color: #fff; text-decoration: none;" href="{{ route('aboutUs') }}">О нас</a></p>
-        <p><a style="color: #fff; text-decoration: none;" href="{{ asset('files/ПОЛЬЗОВАТЕЛЬСКОЕ_СОГЛАШЕНИЕ.pdf') }}" target="_blank">Политика конфиденциальности</a></p>
-        <p><a style="color: #fff; text-decoration: none;" @if(Auth::check() && Auth::user()->user_type == 'partner') href="{{ asset('files/публичная_оферта_партнера.pdf') }}" @else href="{{ asset('files/публичная_оферта.pdf') }}" @endif target="_blank">Публичная оферта</a></p>
+        <p><a style="color: #fff; text-decoration: none;" href="{{ asset('files/Политика конфиденциальности.docx') }}" target="_blank">Политика конфиденциальности</a></p>
+        <p><a style="color: #fff; text-decoration: none;" @if(Auth::check() && Auth::user()->user_type == 'partner') href="{{ asset('files/Публичная оферта.docx') }}" @else href="{{ asset('files/Публичная оферта.docx') }}" @endif target="_blank">Публичная оферта</a></p>
+        <p><a style="color: #fff; text-decoration: none;" href="{{ asset('files/Политика возврата и обмена товаров.docx') }}" target="_blank">Политика возврата и обмена товаров</a></p>
+        <p><a style="color: #fff; text-decoration: none;" href="{{ asset('files/Политика проведения онлайн-платежей.docx') }}" target="_blank">Политика проведения онлайн-платежей</a></p>
+        <p><a style="color: #fff; text-decoration: none;" href="{{ asset('files/Юридическая информация.docx') }}" target="_blank">Юридическая информация</a></p>
     </div>
 
     <div style="font-size: 40px; display: flex; align-items: center;">
