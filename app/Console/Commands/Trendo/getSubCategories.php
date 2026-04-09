71273f8c434b2814b3452229aa6556a32ccf6c7d
Kairat Ubukulov <ubukulov@gmail.com>
2026-04-09 15:00:57 +0500

    fx

--- app/Console/Commands/Trendo/getSubCategories.php
+++ app/Console/Commands/Trendo/getSubCategories.php
@@ -58,6 +58,7 @@ class getSubCategories extends Command
                         ]);
                     }
                 }
+                sleep(2000);
             }
 
             DB::commit();
