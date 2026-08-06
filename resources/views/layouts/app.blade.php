9029b700460f6d5e1434db5a29d78f7e8763f76c
krakotay <krakotay@yandex.ru>
2026-08-06 19:45:50 +0300

    fix: css styles

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -37,7 +37,7 @@
     <script src="{{ asset('js/home.js') }}"></script>
 
     {{-- Внешние библиотеки (Tailwind и Alpine с фиксом defer) --}}
-    <script src="{{ asset('css/tailwindcss.css') }}"></script>
+    <link rel="stylesheet" href="{{ asset('css/tailwindcss.css') }}">
     <script src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js" defer></script>
     <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
 
