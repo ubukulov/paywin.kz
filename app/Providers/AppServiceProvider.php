7d546490cec6fef00d7206d6a41b1bb24083a8cd
Kairat Ubukulov <ubukulov@gmail.com>
2026-08-13 20:44:54 +0500

    fx

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -49,7 +49,7 @@ class AppServiceProvider extends ServiceProvider
             $view->with('currentCity', $currentCity);
         });
 
-        View::composer('*', function($view){
+        View::composer(['layouts.app', 'partner.partner', 'partner.product.create', 'partner.product.edit'], function($view){
             $categories = Cache::remember('product_categories_all', 86400, function () {
                 return ProductCategory::all();
             });
